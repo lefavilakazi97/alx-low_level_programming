6c4a394149cfb09bbdcda898dface7cd9f79ebbf
lefavilakazi97 <lefa.vilakazi97@gmail.com>
2022-12-13 03:11:31 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,22 @@
 /**
  * main - entry point
  *
- * Description: Prints Holberton with _putchar
  *
  * Return: always 0
- */
+*/
 
 int main(void)
 {
-	_putchar("\n");
+	int i = 0;
+	char c;
+	char s[] = "_putchar\n";
+
+	while (i <= 8)
+	{
+		c = s[i];
+		_putchar(c);
+		i++;
+	}
 	return (0);
 }
+
