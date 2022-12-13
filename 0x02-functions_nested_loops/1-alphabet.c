7f7872af7c68f3bb5baded7e2a2674b137a443ae
lefavilakazi97 <lefa.vilakazi97@gmail.com>
2022-12-13 09:47:43 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Prints the alphabet with _putchar
+ *
+ * Return: void
+ */
+
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}	return (0)
+
